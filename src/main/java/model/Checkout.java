5b25627a80d306feb0b2d7e77f9080ec72307875
Roque Serantes <roquesu@inditex.com>
2021-08-09 08:37:42 +0200

    Add Checkout entity

---
+++ src/main/java/model/Checkout.java
@@ -0,0 +1,12 @@
+package model;
+
+import rules.PricingRules;
+
+public class Checkout {
+
+    private PricingRules pricingRules;
+
+    public Checkout(PricingRules pricingRules) {
+        this.pricingRules = pricingRules;
+    }
+}
