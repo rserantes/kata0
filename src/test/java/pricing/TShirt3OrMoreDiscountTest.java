3e1c56d22af70737d1cb49f08310a5c57386cd7d
Roque Serantes <roquesu@inditex.com>
2021-08-09 21:42:47 +0200

    Add shirts discount tests

---
+++ src/test/java/pricing/TShirt3OrMoreDiscountTest.java
@@ -0,0 +1,63 @@
+package pricing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import product.Product;
+import product.ProductFactory;
+
+class TShirt3OrMoreDiscountTest {
+
+    private PricingRule shirtsDiscount = new TShirt3OrMoreDiscount();
+
+    @Test
+    @DisplayName("Zero eligible shirts when only one on checkout")
+    void zeroEligibleWhenOneVoucher() {
+        List<Product> products = new ArrayList<>();
+        products.add(ProductFactory.getTShirt());
+
+        assertEquals(0, shirtsDiscount.eligibleItems(products));
+        assertEquals(0, shirtsDiscount.getDiscount(products));
+    }
+
+    @Test
+    @DisplayName("Three eligible shirts when three on checkout")
+    void oneEligibleWhenTwoVouchers() {
+        List<Product> products = new ArrayList<>();
+        products.add(ProductFactory.getTShirt());
+        products.add(ProductFactory.getTShirt());
+        products.add(ProductFactory.getTShirt());
+
+        assertEquals(3, shirtsDiscount.eligibleItems(products));
+        assertEquals(300, shirtsDiscount.getDiscount(products));
+    }
+
+    @Test
+    @DisplayName("25 eligible shirts when 25 on checkout")
+    void severalEligible() {
+        List<Product> products = new ArrayList<>();
+        for (int i = 0; i < 25; i++) {
+            products.add(ProductFactory.getTShirt());
+        }
+
+        assertEquals(25, shirtsDiscount.eligibleItems(products));
+        assertEquals(2500, shirtsDiscount.getDiscount(products));
+    }
+
+    @Test
+    @DisplayName("25 eligible shirts when 25 on checkout and other items")
+    void severalEligibleWithOtherItems() {
+        List<Product> products = new ArrayList<>();
+        for (int i = 0; i < 25; i++) {
+            products.add(ProductFactory.getTShirt());
+            products.add(ProductFactory.getPants());
+        }
+
+        assertEquals(25, shirtsDiscount.eligibleItems(products));
+        assertEquals(2500, shirtsDiscount.getDiscount(products));
+    }
+
+}
