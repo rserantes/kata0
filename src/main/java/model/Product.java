69ec96974df935a6305c8bd40260305f0edcd469
Roque Serantes <roquesu@inditex.com>
2021-08-09 08:12:54 +0200

    Add Product model

---
+++ src/main/java/model/Product.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Product {
+
+    private String name;
+
+    private Integer amount;
+
+    public Product(String name, Integer amount) {
+        this.name = name;
+        this.amount = amount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+}
