5f2ce9613e2fc5ebe4f1c60fbf2107e37470b51d
Roque Serantes <roquesu@inditex.com>
2021-08-09 21:32:17 +0200

    Add example 4 test

--- src/test/java/model/CheckoutTest.java
+++ src/test/java/model/CheckoutTest.java
@@ -63,4 +63,17 @@ class CheckoutTest {
         assertEquals("81.00 €", checkout.getTotal());
     }
 
+    @Test
+    @DisplayName("Example 4 should cost 74.50 €")
+    void example4() {
+        checkout.scan(ProductFactory.getVoucher());
+        checkout.scan(ProductFactory.getTShirt());
+        checkout.scan(ProductFactory.getVoucher());
+        checkout.scan(ProductFactory.getVoucher());
+        checkout.scan(ProductFactory.getPants());
+        checkout.scan(ProductFactory.getTShirt());
+        checkout.scan(ProductFactory.getTShirt());
+        assertEquals("74.50 €", checkout.getTotal());
+    }
+
 }
