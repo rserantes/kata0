bd83ec27d9b73840a26f2471730c139e489134f4
Roque Serantes <roquesu@inditex.com>
2021-08-09 08:51:03 +0200

    Add Checkout scan method

--- src/main/java/model/Checkout.java
+++ src/main/java/model/Checkout.java
@@ -1,12 +1,24 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
 import rules.PricingRules;
 
 public class Checkout {
 
     private PricingRules pricingRules;
 
+    private List<Product> products = new ArrayList<>();
+
     public Checkout(PricingRules pricingRules) {
         this.pricingRules = pricingRules;
+    }
+
+    public void scan(Product product) {
+        products.add(product);
+    }
+
+    public Integer getProductsQuantity() {
+        return products.size();
     }
 }
---
+++ src/test/java/model/CheckoutTest.java
@@ -0,0 +1,27 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import rules.PricingRules;
+
+class CheckoutTest {
+
+    private Checkout checkout;
+
+    @BeforeEach
+    void setup() {
+        checkout = new Checkout(new PricingRules());
+    }
+
+    @Test
+    @DisplayName("Scanning a product adds it to the current checkout")
+    void scanningAProductAddsIt() {
+        assertEquals(0, checkout.getProductsQuantity());
+        checkout.scan(ProductFactory.getPants());
+        assertEquals(1, checkout.getProductsQuantity());
+    }
+
+}
